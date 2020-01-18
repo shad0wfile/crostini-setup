be69851f37165343cdd468f0a9ff8a6fdeb48ee4
shad0wfile <daniel.lussier@gmail.com>
2020-01-18 09:42:40 -0500

    Updated to add a handful of other apps, and removed deprecated features

--- mybuild-crostini.sh
+++ mybuild-crostini.sh
@@ -12,7 +12,7 @@ echo -n "Atom"
 echo ""
 echo -n "Sublime Text"
 echo ""
-echo -n "xfce4-terminal"
+echo -n "gnome-terminal"
 echo ""
 echo -n "gnome-tweak-tool"
 echo ""
@@ -20,7 +20,7 @@ echo -n "dnsutils"
 echo ""
 echo -n "host"
 echo ""
-echo -n "exiftool"
+echo -n "exiftool & binutils"
 echo ""
 echo -n "nano"
 echo ""
@@ -30,6 +30,10 @@ echo -n "telnet"
 echo ""
 echo -n "nmap"
 echo ""
+echo -n "remmina"
+echo ""
+echo -n "other, adapta-gtk-theme, paper-icons, dracula termianl theme"
+echo ""
 echo ""
  sleep 3
 
@@ -38,40 +42,46 @@ echo ""
 
 ## Install time
 	# Atom
-	wget https://atom.io/download/deb -O atom.deb && sudo apt-get install ./atom.deb && rm /home/$USER/atom.deb
+	wget https://atom.io/download/deb -O atom.deb && sudo apt-get install ./atom.deb && rm $HOME/atom.deb
 	# Sublime Text
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt-get update && sudo apt-get install sublime-text -y
 	# xfce4-terminal
-	sudo apt-get install xfce4-terminal -y
+	sudo apt-get install gnome-terminal -y
 	# gnome-tweak-tool
 	sudo apt-get install gnome-tweak-tool -y
 	# DNSUtils
-	sudo apt-get install dnsutils -y
+	sudo apt-get install dnsutils && sudo apt-get install whois -y
 	# host
 	sudo apt-get install host -y
 	# exiftool
-	sudo apt-get install exiftool -y
+	sudo apt-get install exiftool -y && sudo apt-get install binutils -y
 	# nano editor
 	sudo apt-get install nano -y
 	# telnet (does not enable telnetd, just so you can run telnet to grab banners/etc from cmd line)
 	sudo apt-get install telnet -y
 	# nmap
 	sudo apt-get install nmap -y
+	# nmap
+	sudo apt-get install remmina -y
+	# other - adapta-gtk-theme, paper-icons, dracula termianl theme
+	sudo apt-get install adapta-gtk-theme -y && sudo apt-get install dconf-cli
+	wget https://launchpadlibrarian.net/425392562/paper-icon-theme_1.5.723-201905252133~daily~ubuntu18.04.1_all.deb
+	sudo dpkg -i $HOME/paper*.deb && sudo apt install -f && rm $HOME/paper*.deb
+	git clone https://github.com/dracula/gnome-terminal && bash $HOME/gnome-terminal/install.sh && rm -rf $HOME/gnome-terminal
 
-clear
 ## Additional details
 echo ""
 echo "-------------------------------------------------------------------------------------------"
-echo -n "- xfce4-terminal can be run from the crostini terminal, and then pinned"
+echo -n "- Make sure you edit /usr/share/applications/org.gnome.Terminal.desktop and replace the Icon= with this: /usr/share/icons/Paper/48x48/apps/gnome-terminal.png"
 echo ""
-echo -n "- xfce4-terminal allows for more adjustments, such as transparent bg, better menu, etc"
-echo ""
-echo -n "- gnome-tweak-tool is run from the command line, I utilize this to enable dark theme"
+echo -n "- gnome-tweak-tool is run from the command line, I utilize this to enable the installed adapta-nokto theme"
 echo ""
 echo -n "- make sure to pin any new applications you frequently use to your shelf"
 echo ""
+echo -n "- the final step completed above is for dracula theme to be installed for gnome-terminal"
+echo ""
 echo "--------------------------------------------------------------------------------------------"
 echo ""
-sleep 3
+sleep 1
